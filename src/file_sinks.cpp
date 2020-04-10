0a71eba35a6bae47f1f1c1190e6daa8364d8a974
Dilshod Mukhtarov <dilshodm@gmail.com>
2020-04-10 23:40:26 +0400

    Implemented rotating_file_compress_sink using rotating_file_sink
    It will call given callback function for compressing rotated log file

--- src/file_sinks.cpp
+++ src/file_sinks.cpp
@@ -18,3 +18,7 @@ template class SPDLOG_API spdlog::sinks::basic_file_sink<spdlog::details::null_m
 #include <spdlog/sinks/rotating_file_sink-inl.h>
 template class SPDLOG_API spdlog::sinks::rotating_file_sink<std::mutex>;
 template class SPDLOG_API spdlog::sinks::rotating_file_sink<spdlog::details::null_mutex>;
+
+#include "spdlog/sinks/rotating_file_compress_sink-inl.h"
+template class spdlog::sinks::rotating_file_compress_sink<std::mutex>;
+template class spdlog::sinks::rotating_file_compress_sink<spdlog::details::null_mutex>;
